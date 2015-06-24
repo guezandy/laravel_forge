14840669891e2ef2529bb7afa90d78d5eb5d7b5b
Andrew Rodriguez <andrew.rodriguez007@gmail.com>
2015-06-24 14:40:26 -0500

    added pass

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'host'     => env('DB_HOST', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '5YJC1McJHiE4b1YPzqEx'),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
